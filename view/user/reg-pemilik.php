722bc54a03b22ca13684b19f330537c5e3383301
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-08-04 17:47:35 +0700

    add: register uttp owner

--- view/user/reg-pemilik.php
+++ view/user/reg-pemilik.php
@@ -1,3 +1,19 @@
+<?php
+require_once "../../api/api.php";
+require_once "../../session/index.php";
+$registered = 1;
+// id user
+$userID = $_SESSION["id"];
+
+if (isset($_POST["simpan"])) {
+    if (registrasiPemilikUTTP($_POST, $userID) == $registered) {
+        $success = true;
+    } else {
+        $failed = true;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <!-- Coding By CodingNepal - codingnepalweb.com -->
 <html lang="en">
@@ -40,7 +56,7 @@
             <a href="./index.php" style="font-size: 0.9rem; color: white; padding: 10px 45px; background-color: #d62828; border: none; border-radius: 10px;">Batal</a>
         </div>
 
-        <form action="#">
+        <form method="POST" enctype="multipart/form-data">
             <div class="form first">
                 <div class="details personal">
                     <span class="title">Identitas</span>
@@ -48,44 +64,44 @@
                     <div class="fields">
                         <div class="input-field">
                             <label>Jenis Pemilik</label>
-                            <select required>
+                            <select name="jenis_pemilik" required>
                                 <option disabled selected>Pilih Jenis
                                     Pemilik</option>
-                                <option>Tangan Pertama</option>
-                                <option>Tangan Kedua</option>
+                                <option value="tangan pertama">Tangan Pertama</option>
+                                <option value="tangan kedua">Tangan Kedua</option>
                             </select>
                         </div>
                         <div class="input-field">
                             <label>No Identitas</label>
-                            <input type="number" placeholder="Masukkan No Identitas" required>
+                            <input type="number" placeholder="Masukkan No Identitas" name="no_identitas" required>
                         </div>
                         <div class="input-field">
                             <label>Jenis Identitas</label>
-                            <select required>
+                            <select name="jenis_identitas" require>
                                 <option disabled selected>Pilih Jenis
                                     Identitas</option>
-                                <option>KTP</option>
-                                <option>SIUP</option>
-                                <option>NPWP</option>
+                                <option value="KTP">KTP</option>
+                                <option value="SIUP">SIUP</option>
+                                <option value="NPWP">NPWP</option>
                             </select>
                         </div>
 
                         <div class="input-field">
                             <label>Nama</label>
-                            <input type="text" placeholder="Masukkan Nama Anda" required>
+                            <input type="text" name="nama" placeholder="Masukkan Nama Anda" required>
                         </div>
                         <div class="input-field">
                             <label>Email</label>
-                            <input type="text" placeholder="Masukkan Email" required>
+                            <input type="text" name="email" placeholder="Masukkan Email" required>
                         </div>
 
                         <div class="input-field">
                             <label>No Hp</label>
-                            <input type="number" placeholder="Masukkan No Hp" required>
+                            <input type="number" name="no_hp" placeholder="Masukkan No Hp" required>
                         </div>
                         <div class="input-file">
                             <label>Upload Berkas (KTP/SIUP/NPWP)</label>
-                            <input type="file" id="myFile" name="filename">
+                            <input type="file" id="myFile" name="berkas">
                         </div>
                     </div>
                 </div>
@@ -96,48 +112,71 @@
                     <div class="fields">
                         <div class="input-field">
                             <label>Alamat</label>
-                            <input type="text" placeholder="Masukkan Alamat" required>
+                            <input type="text" name="alamat" placeholder="Masukkan Alamat" required>
                         </div>
 
                         <div class="input-field">
                             <label>RT/RW</label>
-                            <input type="text" placeholder="RT/RW" required>
+                            <input type="text" name="rt/rw" placeholder="RT/RW" required>
                         </div>
 
                         <div class="input-field">
                             <label>Provinsi</label>
-                            <input type="text" placeholder="Masukkan Provinsi" required>
+                            <input type="text" name="provinsi" placeholder="Masukkan Provinsi" required>
                         </div>
 
                         <div class="input-field">
                             <label>Kabupaten</label>
-                            <input type="text" placeholder="Masukkan Kabupaten" required>
+                            <input type="text" name="kabupaten" placeholder="Masukkan Kabupaten" required>
                         </div>
 
                         <div class="input-field">
                             <label>Kecamatan</label>
-                            <input type="text" placeholder="Masukkan Kecamatan" required>
+                            <input type="text" name="kecamatan" placeholder="Masukkan Kecamatan" required>
                         </div>
 
                         <div class="input-field">
                             <label>Kelurahan/Desa</label>
-                            <input type="text" placeholder="Masukkan Kecamatan" required>
+                            <input type="text" name="kelurahan" placeholder="Masukkan Kecamatan" required>
                         </div>
                     </div>
 
                     <div class="button-footer">
-                        <button class="nextBtn" style="margin-right: 50px;">
+                        <button class="nextBtn" name="simpan" style="margin-right: 50px;">
                             <span class="btnText">Simpan</span>
                             <i class="uil uil-navigator"></i>
                         </button>
                     </div>
                 </div>
             </div>
-
         </form>
     </div>
 
     <script src="script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
-
 </html>
+
+<!-- notifikasi sukses -->
+<?php if (isset($success)) : ?>
+    <script>
+        Swal.fire({
+            title: "Berhasil",
+            text: "Pemilik berhasil didaftarkan",
+            icon: "success",
+            button: "OK",
+        })
+    </script>
+<?php endif ?>
+
+<!-- notifikasi gagal -->
+<?php if (isset($failed)) : ?>
+    <script>
+        Swal.fire({
+            title: "Gagal",
+            text: "Registrasi pemilik gagal",
+            icon: "error",
+            button: "OK",
+        })
+    </script>
+<?php endif ?>
