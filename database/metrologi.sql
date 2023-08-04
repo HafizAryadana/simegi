6aa51ad96acf96b65a7ae754acaa32aca225255a
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-08-04 17:47:04 +0700

    add: update database structure

--- database/metrologi.sql
+++ database/metrologi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 26, 2023 at 07:24 PM
+-- Generation Time: Aug 04, 2023 at 12:45 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `pemilik` (
   `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
   `jenis_pemilik` text NOT NULL,
   `no_identitas` text NOT NULL,
   `jenis_identitas` text NOT NULL,
@@ -43,6 +44,14 @@ CREATE TABLE `pemilik` (
   `kelurahan` text NOT NULL,
   `berkas_pemilik` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pemilik`
+--
+
+INSERT INTO `pemilik` (`id`, `user_id`, `jenis_pemilik`, `no_identitas`, `jenis_identitas`, `nama`, `email`, `no_hp`, `alamat`, `rt_rw`, `provinsi`, `kabupaten`, `kecamatan`, `kelurahan`, `berkas_pemilik`) VALUES
+(1, 2, 'tangan pertama', '34234242323', 'SIUP', 'dhvfhdfvjh', 'hfdjvhbjh', '7264823768', 'jsdbjhb', 'hbfdjhvbj', 'jdhfvbjh', 'dhfbvjh', 'bjsdbvjbqjvb', 'hdfjb', '64ccd3a8cf5e3.jpg'),
+(2, 2, 'tangan pertama', '37538538583', 'SIUP', 'hdfhdjkfhvj', 'dhfghdjfdhjh', '82374923749', 'fhgjdhg', 'gfudgu', 'yguygvu', 'eygfudfygu', 'ywgfuyegu', 'ygfuregu', '64ccd65b7cab2.jpg');
 
 -- --------------------------------------------------------
 
@@ -63,7 +72,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nama`, `email`, `password`, `level`) VALUES
-(1, 'admin', 'admin@gmail.com', '12345', 'admin');
+(1, 'admin', 'admin@gmail.com', '12345', 'admin'),
+(2, 'test', 'test@gmail.com', '12345', 'user'),
+(3, 'test lagi', 'testlagi@gmail.com', '12345', 'user'),
+(4, 'rezatest', 'reza@gmail.com', '12345', 'user');
 
 -- --------------------------------------------------------
 
@@ -73,6 +85,7 @@ INSERT INTO `user` (`id`, `nama`, `email`, `password`, `level`) VALUES
 
 CREATE TABLE `uttp` (
   `id` int(11) NOT NULL,
+  `pemilik_id` int(11) DEFAULT NULL,
   `id_uttp` text NOT NULL,
   `uttp_besaran` text NOT NULL,
   `jenis_uttp` text NOT NULL,
@@ -84,6 +97,16 @@ CREATE TABLE `uttp` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `uttp`
+--
+
+INSERT INTO `uttp` (`id`, `pemilik_id`, `id_uttp`, `uttp_besaran`, `jenis_uttp`, `range_kapasitas`, `merk`, `tipe`, `no_seri`, `berkas_uttp`) VALUES
+(6, 1, '3423', 'waktu', 'sdbfskj', 'bfjbj', 'b', 'bwj', '423', '64ccd58f432bd.jpg'),
+(7, 1, '12123', 'panjang', 'sfhdkjh', 'dhfigudhfiuh', 'hdfgiuhdifh', 'gieruhiuh', '834759347593', '64ccd5ae39f32.jpg'),
+(8, 1, '324', 'volume', 'dfbvjdhfbg', 'hdfbvjhb', 'hdbvjhb', 'vhdbjhb', '93248209', '64ccd5ff93de0.jpg'),
+(9, 1, '2323', 'panjang', 'qwerwer', 'werw', 'ewrew', 'ewrfw', '24534', '64ccd63e35738.jpg');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -91,7 +114,8 @@ CREATE TABLE `uttp` (
 -- Indexes for table `pemilik`
 --
 ALTER TABLE `pemilik`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user` (`user_id`);
 
 --
 -- Indexes for table `user`
@@ -103,7 +127,8 @@ ALTER TABLE `user`
 -- Indexes for table `uttp`
 --
 ALTER TABLE `uttp`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_pemilik` (`pemilik_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -113,19 +138,35 @@ ALTER TABLE `uttp`
 -- AUTO_INCREMENT for table `pemilik`
 --
 ALTER TABLE `pemilik`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `uttp`
 --
 ALTER TABLE `uttp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pemilik`
+--
+ALTER TABLE `pemilik`
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `uttp`
+--
+ALTER TABLE `uttp`
+  ADD CONSTRAINT `fk_pemilik` FOREIGN KEY (`pemilik_id`) REFERENCES `pemilik` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
